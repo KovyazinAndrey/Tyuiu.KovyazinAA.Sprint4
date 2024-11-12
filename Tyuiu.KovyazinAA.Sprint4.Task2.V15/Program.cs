0f746afbfcfed157483d4e1dd76d1a7581d36e97
KrovavyiParen <hiroazora@gmail.com>
2024-11-12 10:39:57 +0500

    Task2

--- Tyuiu.KovyazinAA.Sprint4.Task2.V15/Program.cs
+++ Tyuiu.KovyazinAA.Sprint4.Task2.V15/Program.cs
@@ -1,41 +1,40 @@
-﻿using Tyuiu.KovyazinAA.Sprint4.Task1.V8.Lib;
-namespace Tyuiu.KovyazinAA.Sprint4.Task1.V8
+﻿using Tyuiu.KovyazinAA.Sprint4.Task2.V15.Lib;
+namespace Tyuiu.KovyazinAA.Sprint4.Task2.V15
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
             DataService ds = new DataService();
             Console.Title = "Спринт #4 | Выполнил: Ковязин А. А. | ПКТб-24-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #4                                                               *");
-            Console.WriteLine("* Тема: Одномерные массивы ( ввод с клавиатуры )                          *");
-            Console.WriteLine("* Задание #1                                                              *");
-            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Тема: Одномерные массивы ( генератор случайных чисел )                  *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
             Console.WriteLine("* Выполнил: Ковязин Андрей Александрович | ПКТб-24-1                      *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Дан одномерный целочисленный массив на 12 элементов заполненный         *");
-            Console.WriteLine("* значениями с клавиатуры в диапазоне от 2 до 9 подсчитать сумму нечетных *");
-            Console.WriteLine("* элементов массива                                                       *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 11 элементов заполненный         *");
+            Console.WriteLine("* случайными в диапазоне от 2 до 7 подсчитать сумму четных элементов      *");
+            Console.WriteLine("* массива.                                                                *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Введите количество элементов массива                                    *");
-            
+
             int len;
             len = Convert.ToInt32(Console.ReadLine());
             int[] nums = new int[len];
 
             for (int i = 0; i < len; i++)
             {
-                Console.Write("Введите значение " + i + " элемента массива: ");
-                nums[i] = Convert.ToInt32(Console.ReadLine());
+                nums[i] = rnd.Next(2, 7);
             }
-            Console.WriteLine();
 
             Console.WriteLine("Массив: ");
-            for (int i = 0;i < len; i++)
+            for (int i = 0; i < len; i++)
             {
                 Console.Write(nums[i] + "\t");
             }
